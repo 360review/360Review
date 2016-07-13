258b73ae27224c3b94cc89b05cfc5071897a4d4d
calin1611 <calin1611@gmail.com>
2016-07-13 23:16:24 +0300

    Review data is validated to not be null and to have the grades' value between (0, 5]

--- app/controllers/review.php
+++ app/controllers/review.php
@@ -1,32 +1,49 @@
 <?php
 
-  class Review {
-    function __construct() {
-      if (isset($_SESSION) && !empty($_SESSION["userId"])) {
-        include VIEWS."review_view.php";
-      } else {
-        header("Location: " . BASE_URL . "home");
-      }
+class Review
+{
+  function __construct()
+  {
+    if (isset($_SESSION) && !empty($_SESSION["userId"])) {
+      include VIEWS . "review_view.php";
+    } else {
+      header("Location: " . BASE_URL . "home");
     }
+  }
 
-    function index() {
+  function index()
+  {
 //      if (isset($_SESSION) && !empty($_SESSION["userId"])) {
 //        include VIEWS."review_view.php";
 //      } else {
 //        header("Location: " . BASE_URL . "home");
 //      }
-    }
+  }
 
-    function postReview() {
-      $grades = json_decode(file_get_contents('php://input'), true);
+  function postReview()
+  {
+    $grades = json_decode(file_get_contents('php://input'), true);
+
+    if (!is_null($grades)) {
 
       require MODELS . "gradesModel.php";
       $gradesModel = new GradesModel();
-
+      $gradesValid = true;
       foreach ($grades as $grade) {
-        $grade["voter_id"] = $_SESSION["userId"];
-        $gradesModel->addGrade($grade);
+        if ($grade["grade"] > 0 && $grade["grade"] <= 5) {
+          $grade["voter_id"] = $_SESSION["userId"];
+        } else {
+          $gradesValid = false;
+          header('X-PHP-Response-Code: 400 - Bad input. Grades out of range', true, 400);
+          break;
+        }
+      }
+      if ($gradesValid == true) {
+        $gradesModel->addGrades($grades);
       }
 
+    } else {
+      header('X-PHP-Response-Code: 400 - Bad input. Null value', true, 400);
     }
   }
+}
