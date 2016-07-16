49dd3416be99130f7a7650fb8cfb939a29f25f59
Roxana <oprisroxana38@yahoo.com>
2016-07-16 13:43:59 +0300

    getSelfGrades and getTeamGrades

--- app/models/GradesModel.php
+++ app/models/GradesModel.php
@@ -12,8 +12,23 @@ class GradesModel extends DB {
     $sql = "INSERT INTO `grades`($columns) VALUES ($values)";
 
     $this->executeQuery($sql);
+  }
 
-//    return $this->db->lastInsertId();
+  function addGrades($grades) { //What one user graded; $grades is an array with all the review data
+    $columns = implode(", ", array_keys($grades[0]));
+
+    foreach ($grades as $row) {
+      $voter_id = $row["voter_id"];
+      $user_id = $row["user_id"];
+      $competence_id = $row["competence_id"];
+      $grade = $row["grade"];
+      $valuesArr[] = "('$voter_id', '$user_id', '$competence_id', '$grade')";
+    }
+
+    $values  = implode(", ", $valuesArr);
+    $sql = "INSERT INTO `grades`($columns) VALUES $values";
+
+    $this->executeQuery($sql);
   }
 
   function getSelfGrades() {
